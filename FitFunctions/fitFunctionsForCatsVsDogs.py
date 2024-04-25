aa8aec9a6ae0e6ef476e7b74a4cc965428407b5a
Karlo Jair Guevara Diaz <karlo.guevara@cimat.mx>
2024-04-25 16:02:04 -0600

    Added the trainerCatsVsDogs class to the repository
    The class `trainerCatsVsDogs` was added, which initializes the data loaders and combines the functions `train_modelCNN` and `getAccuracy_and_MAE` to train the cats vs dogs models. And we test that class in the 'testDataSet_Cats_vs_Dogs'.

--- FitFunctions/fitFunctionsForCatsVsDogs.py
+++ FitFunctions/fitFunctionsForCatsVsDogs.py
@@ -1,8 +1,11 @@
 
+import os
 import torch
 import torch.nn as nn
-import os
 from tqdm import tqdm
+from torch import optim
+from torch.utils.data import DataLoader
+
 
 
 
@@ -15,7 +18,7 @@ def train_modelCNN(data_loader, model, opt_model, device,
         Fit function for a clasificator cat vs dog model. This function will 
         save the models in each epoch, and it return the history if we whant.
 
-        Parameters
+        Attributes
         -----------
         data_loader : DataLoader  
             index list of the batch tensors of the dataset
@@ -104,6 +107,7 @@ def train_modelCNN(data_loader, model, opt_model, device,
 
         #todo save the best model
         #* Save the model afther the epoch
+        print("model_save_dir =", model_save_dir)
         torch.save({ 
                 'model_state_dict': model.state_dict(), 
                 'optimizer_state_dict': opt_model.state_dict(), 
@@ -127,6 +131,7 @@ def getAccuracy_and_MAE(model, data_loader, criterion, batch_size, device):
             loss function of the model
         batch_size : int
             Size of the batchs in the data loader
+
         Returns
         -------
         Return a tuple with the accuracy of the model in the data loader and the
@@ -160,4 +165,200 @@ def getAccuracy_and_MAE(model, data_loader, criterion, batch_size, device):
     return model_ACC, model_MAE
 
 
+class trainerCatsVsDogs:
+    '''
+        This class trains a classifier model for distinguishing between cats and dogs. 
+        It saves the models at the end of each epoch and returns the training history.
 
+        Attributes
+        -----------
+        model : nn.Module
+            The model to be trained.
+        dataSet : Dataset
+            The dataset containing the training data.
+        device : str
+            The device (e.g., 'cuda' or 'cpu') where the training will take place.
+        batch_size : int, optional
+            The batch size used for training (default is 64).
+        dataSet_Val : Dataset, optional
+            The dataset containing the validation data (default is None).
+        model_save_dir : str, optional
+            The directory where trained models will be saved (default is None).
+
+        Methods
+        -------
+        fitModel(opt_model  : torch.optim, 
+                criterion  : torch.nn.Module = torch.nn.CrossEntropyLoss(),
+                num_epochs : int = 1)
+            Train the model for the specified number of epochs.
+
+        Returns
+        -------
+        history : dict or None
+            A dictionary containing the training history (if get_History is True), otherwise None.
+            The history includes the following metrics:
+            - "train_MAE": List of training mean absolute error (MAE) for each epoch.
+            - "train_ACC": List of training accuracy for each epoch.
+            - "val_MAE": List of validation MAE for each epoch (if validation data is provided).
+            - "val_ACC": List of validation accuracy for each epoch (if validation data is provided).
+    '''
+
+    def __init__(self, 
+                model : nn.Module, 
+                dataSet, 
+                device : str = "cpu",
+                batch_size : int = 64,
+                dataSet_Val = None, 
+                model_save_dir : str =  None):
+
+        if(model_save_dir is None):
+            raise ValueError('model_save_dir could not be None') 
+
+        #* Initialize history to store training metrics
+        self.history = {  
+            "train_MAE" : [],
+            "train_ACC" : [],
+            "val_ACC"   : [],
+            "val_MAE"   : []
+            }
+
+        #* Initialize class attributes
+        self.dataSet     = dataSet
+        self.model       = model
+        self.device      = device
+        self.batch_size  = batch_size
+        self.dataSet_Val = dataSet_Val
+        self.model_save_dir  = model_save_dir
+
+        #* Initialize data loaders
+        self.data_loader = DataLoader(
+                                self.dataSet, 
+                                batch_size  = self.batch_size,
+                                num_workers = 0,
+                                shuffle = True)
+        
+        if(self.dataSet_Val != None):
+            self.data_loader_Val = DataLoader(
+                                    self.dataSet_Val, 
+                                    batch_size  = self.batch_size,
+                                    num_workers = 0,
+                                    shuffle = True)
+
+    
+    def getAccuracy_and_MAE(self, criterion : torch.nn.Module, data_loader : DataLoader):
+        '''
+            Parameters
+            ----------
+            data_loader : DataLoader  
+                index list of the batch tensors of the dataset
+            criterion :  torch.nn.Module, optional
+                loss function of the model
+            device : str
+                Device that we use like GPU
+
+            Returns
+            -------
+            Return a tuple with the accuracy of the model in the data loader and the
+            model total loss in the data loader.
+        '''
+
+        #* Set the model to evaluation mode
+        self.model.eval()
+        model_ACC = 0
+        model_MAE = 0
+        size_data_loader = len(data_loader.dataset)
+
+        #* Disable gradient computation
+        with torch.no_grad(): 
+            for imgs, labels in data_loader:
+                labels  = labels.to(self.device)
+                imgs    = imgs.to(self.device)
+                outputs = self.model(imgs)
+
+                batch_loss = criterion(outputs, labels)  
+                model_MAE += batch_loss.item()*self.batch_size
+
+                #* Get the index of the maximum predicted value
+                prediction = torch.max(outputs, 1)[1]  
+                #* Count the number of correct predictions
+                model_ACC += (prediction == labels).sum().item()  
+
+        model_ACC = model_ACC / size_data_loader  # Compute the accuracy
+        model_MAE = model_MAE / size_data_loader
+
+        return model_ACC, model_MAE
+
+    def fitModel(self, 
+                opt_model  : torch.optim, 
+                criterion  : torch.nn.Module = torch.nn.CrossEntropyLoss(),
+                num_epochs : int = 1):
+        '''
+            Train the model for the specified number of epochs using criterion, and opt_model.
+            
+            Parameters
+            ----------
+            opt_model : optim
+                The optimization algorithm used for training.
+            criterion : torch.nn.Module, optional
+                The loss function used for training (default torch.nn.CrossEntropyLoss()).
+            num_epochs : int, optional
+                The number of epochs to train the model (default is 1).
+        '''
+
+        if num_epochs < 0:
+            raise ValueError('num_epochs should be non-negative') #todo test
+
+        sizeDataSet = len(self.data_loader.dataset) 
+        self.model.to(self.device)
+
+
+        for epoch in range(num_epochs):
+            loop = tqdm(enumerate(self.data_loader), total = len(self.data_loader))
+            self.model.train()#* Put model in training mode
+            train_MAE = 0 
+            train_ACC = 0 
+
+            for batch_idx, (imgs, labels) in loop:
+                imgs    =   imgs.to(self.device)
+                labels  = labels.to(self.device)
+                opt_model.zero_grad()
+                outputs = self.model(imgs)
+
+                #* Get the preditions and the loss for the batch
+                loss       = criterion(outputs, labels)  
+                train_MAE += loss.item()*self.batch_size  
+                prediction = torch.max(outputs, 1)[1]  
+                train_ACC += (prediction == labels).sum().item()
+
+                #* Get gradients, and update the parameters of the model
+                loss.backward()
+                opt_model.step()
+
+                #* Plot the loss and the progress bar
+                loop.set_description(f"Epoch {epoch+1}/{num_epochs} process: {int((batch_idx / len(self.data_loader)) * 100)}")
+                loop.set_postfix(modelLoss = loss.data.item())
+
+            train_ACC = train_ACC / sizeDataSet
+            train_MAE = train_MAE / sizeDataSet
+            print(f'Epoch completed, TRAIN MAE: {train_MAE:.4f}')
+            print(f'Epoch completed, TRAIN ACC: {train_ACC:.4f}')
+            self.history["train_MAE"].append(train_MAE) 
+            self.history["train_ACC"].append(train_ACC)
+
+            #* Get the ACC, and MAE in val dataSet and plot them, if we have validation data.
+            if(self.dataSet_Val != None):
+                ACC_Val, MAE_Val = self.getAccuracy_and_MAE( 
+                                                    data_loader = self.data_loader_Val, 
+                                                    criterion   = criterion, 
+                                                    )
+
+                print(f'Epoch completed, VAL MAE: {(MAE_Val):.4f}')
+                print(f'Epoch completed, VAL ACC: {(ACC_Val):.4f}')
+                self.history["val_MAE"].append(MAE_Val)
+                self.history["val_ACC"].append(ACC_Val)
+
+            #* Save the model every epoch
+            torch.save({ 
+                'model_state_dict': self.model.state_dict(), 
+                'optimizer_state_dict': opt_model.state_dict(), 
+            }, os.path.join(self.model_save_dir, f'checkpoint_epoch_{epoch + 1}.pt'))
