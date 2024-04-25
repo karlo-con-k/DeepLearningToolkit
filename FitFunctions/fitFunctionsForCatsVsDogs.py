a6d51ff461021264f66bef2d855921eb1626011b
karlo-con-k <karlo.guevara@cimat.mx>
2024-04-24 18:46:29 -0600

    The function 'getAccuracy_and_MAE' from the file 'fitFunctionsForCatsVsDogs' was tested. Additionally, the calculation of ACC was added in train_modelCNN.

--- FitFunctions/fitFunctionsForCatsVsDogs.py
+++ FitFunctions/fitFunctionsForCatsVsDogs.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 def train_modelCNN(data_loader, model, opt_model, device, 
                     data_loader_Val = None, 
                     num_epochs = 1, criterion = torch.nn.CrossEntropyLoss(), 
-                    get_History = True, getValLoos = False,
+                    get_History = True, getVal = False,
                     model_save_dir = None):
     '''
         Fit function for a clasificator cat vs dog model. This function will 
@@ -17,7 +17,6 @@ def train_modelCNN(data_loader, model, opt_model, device,
 
         Parameters
         -----------
-
         data_loader : DataLoader  
             index list of the batch tensors of the dataset
         model : nn.Module
@@ -34,8 +33,8 @@ def train_modelCNN(data_loader, model, opt_model, device,
             loss function of the model
         get_History : bool, optional
             if we whant to get the losst historial of the model 
-        getValLoos : bool, optional 
-            if we whant to get the loos in the validation dataset during the training  
+        getVal : bool, optional 
+            If we whant to get the ACC and MAE in validation data loader
         model_save_dir : str, optional
             directory to save the trained model (default is None)
 
@@ -44,66 +43,64 @@ def train_modelCNN(data_loader, model, opt_model, device,
             history of the training process, if get_History is True, otherwise None
     '''
     
-    if(get_History):
-        history = {
-                    "MAE" : [],
-                    "ACC" : [] #todo add the ACC
-                    }
-        if(getValLoos == True):
-            history['MAE_Val'] = []
-    
-    model.to(device)
-    model.train()
-    sizeDataSet = len(data_loader.dataset)              #* size of the dataSet i.e number of images
-    
-    if(data_loader_Val is not None):
-        sizeDataSetVal = len(data_loader_Val.dataset)   #* size of the dataSetVal 
-    else:
-        sizeDataSetVal = 1
 
+    model.to(device)
+    
+    sizeDataSet = len(data_loader.dataset) 
     batch_size  = data_loader.batch_size
+    if(get_History or getVal):
+        history = {
+                    "train_MAE" : [],
+                    "train_ACC" : [] #todo add the ACC
+                    }
+        if(getVal == True):
+            history['val_MAE'] = []
+            history['val_ACC'] = []
 
 
     for epoch in range(num_epochs):
-
-        #* This will be the Mean Absolute Error of the dataSet 
-        model_total_loss  = 0
+        model.train()
+        train_MAE = 0 #* This will be the MAE of the train dataSet
+        train_ACC = 0  #* This will be the ACC of the train dataSet
         loop = tqdm(enumerate(data_loader), total = len(data_loader))
-        for batch_idx, (img, label) in loop:
-            img        = img.to(device)
-            label      = label.to(device) #! label = label.float().to(device) 
+        
+        for batch_idx, (imgs, labels) in loop:
+            imgs        = imgs.to(device)
+            labels      = labels.to(device) #! labels = labels.float().to(device) 
             opt_model.zero_grad()
-            prediction = model(img)
-            loss  = criterion(prediction, label)
-            #* loss.item() for get the value of the tensor.
-            model_total_loss += loss.item()*batch_size      
+            outputs = model(imgs)
+
+            #* Get the preditions and the loss for the batch
+            loss              = criterion(outputs, labels)  #* get the value of the tensor.
+            train_MAE += loss.item()*batch_size #Todo flotant train_ACC problems ??? 
+            prediction = torch.max(outputs, 1)[1]  
+            train_ACC  += (prediction == labels).sum().item() 
+
             loss.backward()
             opt_model.step()
             loop.set_description(f"Epoch {epoch+1}/{num_epochs} process: {int((batch_idx / len(data_loader)) * 100)}")
             loop.set_postfix(modelLoss = loss.data.item())
 
-        avg_loss = model_total_loss / sizeDataSet
-        print(f'Epoch completed, Average Loss: {avg_loss:.4f}')
+        train_ACC = train_ACC / sizeDataSet
+        train_MAE = train_MAE / sizeDataSet
+        print(f'Epoch completed, Average Loss in TRAIN (MAE): {train_MAE:.4f}')
+        print(f'Epoch completed, Average accuracy in TRAIN (ACC): {train_ACC:.4f}')
 
         if(get_History): 
-            history["MAE"].append(model_total_loss/sizeDataSet) 
-            #TODO define well the ACC
-            history["ACC"].append(model_total_loss/sizeDataSet) 
+            history["train_MAE"].append(train_MAE) 
+            history["train_ACC"].append(train_ACC) 
 
-        #TODO ADD Val ACC
-        if(getValLoos == True):
-            model_total_loss_Val = 0
-            for idx, (img, label) in enumerate(data_loader_Val):
-                img        = img.to(device)
-                label      = label.to(device)
-                prediction = model(img)
-                loss       = criterion(prediction, label)      
-                model_total_loss_Val += loss.item()*batch_size     
+        if(getVal == True):
+            ACC_Val, MAE_Val = getAccuracy_and_MAE( model = model, 
+                                                    data_loader = data_loader_Val, 
+                                                    criterion   = criterion, 
+                                                    batch_size  = batch_size, 
+                                                    device      = device)
 
-            if(get_History):
-                history["MAE_Val"].append(model_total_loss_Val/sizeDataSetVal)
-
-            print(f'Epoch completed, Average Val Loss: {(model_total_loss_Val/sizeDataSetVal):.4f}')
+            print(f'Epoch completed, MAE in VAL: {(MAE_Val):.4f}')
+            print(f'Epoch completed, ACC in VAL: {(ACC_Val):.4f}')
+            history["val_MAE"].append(MAE_Val)
+            history["val_ACC"].append(ACC_Val)
 
         #* Save the model afther the epoch
         torch.save({ 
@@ -111,11 +108,11 @@ def train_modelCNN(data_loader, model, opt_model, device,
                 'optimizer_state_dict': opt_model.state_dict(), 
             }, os.path.join(model_save_dir, f'checkpoint_epoch_{epoch + 1}.pt'))
 
-    if(get_History):
+    if(get_History or getVal):
         return history
 
 
-def getAccuracy(model, data_loader, device):
+def getAccuracy_and_MAE(model, data_loader, criterion, batch_size, device):
     '''
         Parameters
         -----------
@@ -125,30 +122,38 @@ def getAccuracy(model, data_loader, device):
             model to fit
         device : str
             Device that we use like GPU
-
+        criterion :  torch.nn.Module, optional
+            loss function of the model
+        batch_size : int
+            Size of the batchs in the data loader
         Returns
         -------
-        Return the accuracy of the model in the data loader
+        Return a tuple with the accuracy of the model in the data loader and the
+        model total loss in the data loader
     '''
 
-    #Todo test this function 
-
-    correct = 0
-    total = 0
     #* Set the model to evaluation mode
     model.eval()
+    model_ACC = 0
+    model_MAE = 0
+    size_data_loader = len(data_loader.dataset)
 
-    with torch.no_grad(): #* Disable gradient computation
+    #* Disable gradient computation
+    with torch.no_grad(): 
         for imgs, labels in data_loader:
-            imgs = imgs.to(device)
-            labels = labels.to(device)
+            labels  = labels.to(device)
+            imgs    = imgs.to(device)
             outputs = model(imgs)
+
+            batch_loss = criterion(outputs, labels)  
+            model_MAE += batch_loss.item()*batch_size
+
             #* Get the index of the maximum predicted value
-            _, predicted = torch.max(outputs, 1)  
-            #* Increment the total count by the batch size
-            total   += labels.size(0)
+            prediction = torch.max(outputs, 1)[1]  
             #* Count the number of correct predictions
-            correct += (predicted == labels).sum().item()  
-    
-    accuracy = correct / total  # Compute the accuracy
-    return accuracy
+            model_ACC += (prediction == labels).sum().item()  
+
+    model_ACC = model_ACC / size_data_loader  # Compute the accuracy
+    model_MAE = model_MAE / size_data_loader
+
+    return model_ACC, model_MAE
