880c8a1ace09a388df2d189a638008ad7b05a755
Karlo Jair Guevara Diaz <karlo.guevara@cimat.mx>
2024-04-29 18:16:54 -0600

    Redefined plot_img_tensor
    The plot_img_tensor function has been redefined; now it takes the channels to be printed as input. Now it's not possible to send channels that are not present in the tensor.

--- TestNotebooks/toolsTest.py
+++ TestNotebooks/toolsTest.py
@@ -1,9 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_img_tensor(tensor, title = "Mi gallo"):
-    image_array = tensor.detach().cpu().numpy()
+def plot_img_tensor(tensor,
+                    plot_channels : list[int] = [0,1,2],
+                    title : str = "my title"):
+    
+    # print(max(plot_channels), tensor.shape[0])
+    if(max(plot_channels) >= tensor.shape[0] or min(plot_channels) < 0):
+        raise ValueError('this channes are not in the tensor') 
+
+    image_array = tensor[plot_channels].detach().cpu().numpy()
     image_array = np.transpose(image_array, (1, 2, 0))
+    print(image_array.shape)
     plt.imshow(image_array)
     plt.axis('off')  # Turn off axis
     plt.title(title)
