5329de1616a22124af40ec16ae22f6b5c7cdeb16
Karlo Jair Guevara Diaz <karlo.guevara@cimat.mx>
2024-05-01 15:09:09 -0600

    Corrected error
    We implemented the model with the intention of using it image by image, and we adjusted it to work with batches of images so that it can be trained with the 'fiterU_Net'class.

--- Models/model_U_Net.py
+++ Models/model_U_Net.py
@@ -27,7 +27,7 @@ class model_U_Net(nn.Module):
                 in_CHANNELS: int = 1):
         super(model_U_Net, self).__init__()
 
-        
+
         self.conv1 = self.conv_block(in_CHANNELS , 64, 3, 1) #TODO add double conv in the conv block for les layers
         self.conv2 = self.conv_block(64, 64, 3, 1)
 
@@ -71,7 +71,7 @@ class model_U_Net(nn.Module):
         self.conv14   = self.conv_block(128, 64, 3, 1)
         self.conv14_1 = self.conv_block(64, 64, 3, 1)
 
-        self.lastConv = self.conv_block(64, 2, 1, 1)
+        self.lastConv = self.conv_block(64, 1, 1, 1)
 
 
     def conv_block(self, 
@@ -113,58 +113,46 @@ class model_U_Net(nn.Module):
         outPut = self.conv1(inPut)
         outPut = self.conv2(outPut)
         copy1  = transforms.Resize((392, 392))(outPut)
-
         outPut = self.maxPol1(outPut) 
 
         outPut = self.conv3(outPut)
         outPut = self.conv4(outPut)
-        copy2  = outPut[:, 40:240, 40:240]
-
+        copy2  = outPut[:, :, 40:240, 40:240]
         outPut = self.maxPol2(outPut)
 
         outPut = self.conv5(outPut)
         outPut = self.conv6(outPut)
-        copy3  = outPut[:, 16:120, 16:120 ] #* must be 104x104x512
+        copy3  = outPut[:, :, 16:120, 16:120] #* must be 104x104x512
         outPut = self.maxPol3(outPut)
 
         outPut = self.conv7(outPut)
         outPut = self.conv8(outPut)
-        copy4  = outPut[:, 4:60, 4:60] #todo maxPol ? 
-
+        copy4  = outPut[:, :, 4:60, 4:60] #todo maxPol ? 
         outPut = self.maxPol4(outPut)
 
         outPut = self.conv9(outPut)
         outPut = self.conv10(outPut)
 
         outPut = self.upConv1(outPut)
-        outPut = torch.cat((outPut, copy4))         #* concatenate the tensors
-        # print("outPut befor up = ", outPut.shape) #* must be 28x28x1024
-        # print("outPut post up = ", outPut.shape)  #* must be 28x28x1024
-
+        outPut = torch.cat((outPut, copy4), dim=1)
         outPut = self.conv11(outPut)
         outPut = self.conv11_1(outPut)
 
         outPut = self.upConv2(outPut)
-        outPut = torch.cat((outPut, copy3))
-
+        outPut = torch.cat((outPut, copy3), dim = 1)         #* concatenate the tensors
         outPut = self.conv12(outPut)
         outPut = self.conv12_1(outPut)
 
         outPut = self.upConv3(outPut)
-        outPut = torch.cat((outPut, copy2))
-
+        outPut = torch.cat((outPut, copy2), dim= 1)
         outPut = self.conv13(outPut)
         outPut = self.conv13_1(outPut)
 
         outPut = self.upConv4(outPut)
-        outPut = torch.cat((outPut, copy1))
-
+        outPut = torch.cat((outPut, copy1), dim= 1)
         outPut = self.conv14(outPut)
         outPut = self.conv14_1(outPut)
         outPut = self.lastConv(outPut)
 
         return outPut
-
-
-
 
