cc4715900cfde7cdce748a117829231e25c2cd0f
Karlo Jair Guevara Diaz <karlo.guevara@cimat.mx>
2024-05-11 13:44:42 -0600

    Add plot enviromnet to 'plot_img_tensor' function.
    Now, within the function, we can specify the environment to plot, enabling multiple plots using this funtion instance for only one image plot.

--- TestNotebooks/toolsTest.py
+++ TestNotebooks/toolsTest.py
@@ -4,7 +4,7 @@ import numpy as np
 def plot_img_tensor(tensor,
                     plot_channels : list[int] = [0,1,2],
                     title    : str = "my title",
-                    localplt : plt.subplots = plt.subplots(1, 1, figsize=(12, 6))):
+                    localplt = None):
     '''
         Function for plot a img using a tensor. 
         The tensor shape need to be [Chanels, Height, Width], and
@@ -18,19 +18,23 @@ def plot_img_tensor(tensor,
             localplt : plt.subplots, optional
                 The enviroment to plot the tensor img
     '''
-    
-    if(max(plot_channels) >= tensor.shape[0] or min(plot_channels) < 0):
+
+    if localplt is None:
+        fig, ax = plt.subplots(1, 1, figsize=(12, 6))
+    else:
+        fig, ax = localplt
+
+    if max(plot_channels) >= tensor.shape[0] or min(plot_channels) < 0:
         print("tensor.shape  = ", tensor.shape)
         print("plot_channels = ", plot_channels)
-        raise ValueError('this channes are not in the tensor')
+        raise ValueError('These channels are not in the tensor')
 
     image_array = tensor[plot_channels].detach().cpu().numpy()
     image_array = np.transpose(image_array, (1, 2, 0))
-    print("image_array.shape = ", image_array.shape)
+
+    ax.imshow(image_array)
+    ax.axis('off')  # Turn off axis
+    ax.set_title(title)
     
-    #TODO test
-    print(localplt.ax)
-    localplt.imshow(image_array)
-    localplt.axis('off')  # Turn off axis
-    localplt.title(title)
-    localplt.show()
+    if localplt is None:
+        plt.show()
